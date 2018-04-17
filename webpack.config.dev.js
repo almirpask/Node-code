4c2175380587f500df17de4893e56fef8ec6a0a5
almirpask <almirpask@hotmail.com>
2018-04-16 21:59:26 -0300

    Initial Commit

---
+++ webpack.config.dev.js
@@ -0,0 +1,23 @@
+import path  from 'path'
+
+export default {
+    // debug: true,
+    devtool: 'inline-source-map',
+    
+    entry: [
+        path.resolve(__dirname, 'src/index')
+    ],
+    target: 'web',
+    output: {
+        path: path.resolve(__dirname, 'src'),
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    plugins: [],
+    module: {
+        rules: [
+            {test: /\.js$/, exclude: /node_modules/, loader: ['babel-loader']},
+            {test: /\.css$/, loader: 'style-loader!css-loader'}
+        ]
+    }
+}
